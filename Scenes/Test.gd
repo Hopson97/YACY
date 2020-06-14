1a8f3c4da450b34c5516af2a44e0eebcd64ec510
Hopson97 <mhopson@hotmail.co.uk>
2020-06-14 09:02:47 +0100

    Load a ranjdom level

--- Scenes/Test.gd
+++ Scenes/Test.gd
@@ -1,12 +1,31 @@
 extends Button
 
+func find_levels():
+	var levels = []
+	var dir = Directory.new()
+	if dir.open("Levels") == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir():
+				levels.append(file_name)
+			file_name = dir.get_next()
+	return levels;
+
 func _ready():
 	print("GDNS Test")
 	##get_parent().get_parent().get_node("LegacyLevel/ObjectLoader").create_wall(Vector2(-30,-30), Vector2(100,100), 1, 1, 1)
 
 func _on_Button_pressed():
 	#get_parent().loadLevelFromLocalhost(2071)
-	get_parent().loadLevelFromFilesystem("user://105515.Global Encounter.ilikegames44.cy")
+	var levels = find_levels()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var level = levels[rng.randi_range(0, levels.size() - 1)]
+	print(level)
+	
+	get_parent().loadLevelFromFilesystem("res://Levels/" + level)
 	get_parent().get_parent().get_node("LegacyLevel").call("spawnPlayer")
 	set_disabled(true)
 	
+
--- scripts/LevelBrowser/Browser.gd
+++ scripts/LevelBrowser/Browser.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func load_level(file):
-	get_parent().loadLevelFromFilesystem("Levels/" + file)
+	get_parent().loadLevelFromFilesystem("res://Levels/" + file)
 	get_parent().get_node("LegacyLevel").call("spawnPlayer")
 
 
@@ -12,14 +12,15 @@ func _ready():
 		var file_name = dir.get_next()
 		var x = 0
 		var y = 0
-		var w = 100
-		var h = 50
+		var w = 500
+		var h = 32
 		while file_name != "":
 			if !dir.current_is_dir():
 				print("Found file: " + file_name)
 				var button = Button.new()
 				button.set_size(Vector2(w, h))
 				button.set_position(Vector2(x, y));
+				button.set_text(file_name)
 				y += h + 10;
 				button.connect("button_down", self, "load_level", [file_name])
 				add_child(button)
